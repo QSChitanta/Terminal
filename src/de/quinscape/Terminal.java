cacb8f1ebd897319741611b75bf565dcbcf4f274
pchitanta <patrick.chitanta@quinscape.de>
2021-12-06 15:21:56 +0100

    Review changes: - add .gitignore - remove methods that enable to write text on existing .txt file - text file name is now 8 characters long

--- src/de/quinscape/Terminal.java
+++ src/de/quinscape/Terminal.java
@@ -1,77 +1,39 @@
 package de.quinscape;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.UUID;
 
 public class Terminal {
-    private static final Scanner scanner = new Scanner(System.in);
-
-    private File savedFile() {
-        return new File("/Users/pchitanta/Documents/JavaCourseUdemy/Terminal/49e7b828-c9a2-46e4-93f5-" +
-                "cbecf45a013c.txt");
-    }
-
-    private boolean doesFileExist(File file) {
-        return file.exists();
-    }
-
     private String fileNameGenerator() {
         UUID code = UUID.randomUUID();
-        return code.toString();
+        return code.toString().substring(0,8);
     }
 
     private void writeText() {
         try {
+            Scanner scanner = new Scanner(System.in);
             boolean isOver = true;
-            if (!doesFileExist(savedFile())) {
-                java.io.FileWriter writer = new java.io.FileWriter(fileNameGenerator() + ".txt");
-                while (isOver) {
-                    String userInput = scanner.nextLine();
-                    if (!userInput.isEmpty()) {
-                        writer.append(userInput).append("\n");
-                    } else {
-                        isOver = false;
-                        writer.close();
-                    }
-                }
-            } else {
-                try (java.io.FileWriter wr = new java.io.FileWriter("49e7b828-c9a2-46e4-93f5-cbecf45a013c.txt", true)) {
-                    while (isOver) {
-                        String userInput = scanner.nextLine();
-                        if (!userInput.isEmpty()){
-                            wr.write(userInput + "\n");
-                        } else {
-                            isOver = false;
-                            wr.close();
-                        }
-                    }
+            java.io.FileWriter writer = new java.io.FileWriter(fileNameGenerator() + ".txt");
+            while (isOver) {
+                String userInput = scanner.nextLine();
+                if (!userInput.isEmpty()) {
+                    writer.append(userInput).append("\n");
+                } else {
+                    isOver = false;
                 }
             }
+            writer.close();
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void printCheckIfFileExists() {
-        if (doesFileExist(savedFile())) {
-            System.out.println("Main File already exists.");
-        } else {
-            System.out.println("Main file does not exist. Creating main...");
-        }
-    }
-
     private void printInstructions() {
-        if (doesFileExist(savedFile())) {
-            System.out.println("Append text to existing file: ");
-        } else {
             System.out.println("Write your text below: ");
-        }
     }
 
     public void startApplication() {
-        printCheckIfFileExists();
         printInstructions();
         writeText();
     }
