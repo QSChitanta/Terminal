5f88894c1c37bea9b6b879144596f92b8af2bb93
pchitanta <patrick.chitanta@quinscape.de>
2021-12-07 23:45:28 +0100

    temporary solution

--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -2,12 +2,20 @@ package de.quinscape;
 
 public class Main {
     public static void main(String[] args) {
+        //args[0] == null, oh i found something haha
         Terminal terminal = new Terminal();
-        if(args.length != 0){
-            terminal.startApplication();
-        } else {
-            terminal.startApplicationWithExistingFile(args[0]);
-        }
-
+        terminal.startApplication();
     }
 }
+
+//        if(args.length != 0){
+//            terminal.startApplication();
+//        } else {
+//            terminal.startApplicationWithExistingFile(args[0]);
+//        }
+//        terminal.startApplicationWithExistingFile("existingFile.txt");
+//    }
+
+//anwendung soll schauen ob es eine datei gibt, über args main
+
+//wenn ich das programm starte, soll ein parameter erstellen , das soll die chooseFile methode ersetzen
--- src/de/quinscape/Terminal.java
+++ src/de/quinscape/Terminal.java
@@ -8,42 +8,60 @@ import java.util.UUID;
 public class Terminal {
     private String fileNameGenerator() {
         UUID code = UUID.randomUUID();
-        return code.toString().substring(0,8);
+        return code.toString().substring(0, 8);
     }
 
-    private FileWriter createNewFile() throws IOException{
+    private FileWriter createNewFile() throws IOException {
         return new FileWriter(fileNameGenerator() + ".txt");
     }
 
-    private void writeText() {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            boolean isOver = true;
-            FileWriter writer = createNewFile();
-            while (isOver) {
-                String userInput = scanner.nextLine();
-                if (!userInput.isEmpty()) {
-                    writer.append(userInput).append("\n");
-                } else {
-                    isOver = false;
-                }
-            }
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+    private FileWriter useGivenFile() throws IOException {
+        return new FileWriter(savedFile(), true);
+    }
+
+    private String savedFile() {
+        return "/Users/pchitanta/Documents/JavaCourseUdemy/Terminal/2cf00dd4.txt";
+    }
+
+    private FileWriter chooseFile() throws IOException{
+        if (savedFile().isEmpty()) {
+            return createNewFile();
+        } else {
+            return useGivenFile();
         }
     }
 
+    private void writeText() {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                boolean isOver = true;
+                FileWriter writer = chooseFile();
+                while (isOver) {
+                    String userInput = scanner.nextLine();
+                    if (!userInput.isEmpty()) {
+                        writer.append(userInput).append("\n");
+                    } else {
+                        isOver = false;
+                    }
+                }
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+    }
+
     private void printInstructions() {
-            System.out.println("Write your text below: ");
+        System.out.println("Write your text below: ");
     }
 
     public void startApplication() {
         printInstructions();
         writeText();
     }
-
-    public void startApplicationWithExistingFile(String arg) {
-
-    }
+//    public void startApplicationWithExistingFile() {
+//    }
+//
+//    public void startProgram(){
+//
+//    }
 }
