6bc347cb0b59a3b291f8f80108f63168049cb0c1
pchitanta <patrick.chitanta@quinscape.de>
2021-12-09 10:41:07 +0100

    Program works as intended

--- src/de/quinscape/Main.java
+++ src/de/quinscape/Main.java
@@ -1,21 +1,17 @@
 package de.quinscape;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        //args[0] == null, oh i found something haha
+    public static void main(String[] args) throws IOException {
         Terminal terminal = new Terminal();
-        terminal.startApplication();
+        System.out.println("(IMPORTANT: If you have a text file already and want to append text, " +
+                "add it to your IDE's main configuration)");
+        if (args.length == 0) {
+            terminal.appendToFile();
+        }
+        if (args.length != 0){
+            terminal.appendToGivenFile(args[0]);
+        }
     }
 }
-
-//        if(args.length != 0){
-//            terminal.startApplication();
-//        } else {
-//            terminal.startApplicationWithExistingFile(args[0]);
-//        }
-//        terminal.startApplicationWithExistingFile("existingFile.txt");
-//    }
-
-//anwendung soll schauen ob es eine datei gibt, über args main
-
-//wenn ich das programm starte, soll ein parameter erstellen , das soll die chooseFile methode ersetzen
--- src/de/quinscape/Terminal.java
+++ src/de/quinscape/Terminal.java
@@ -2,66 +2,66 @@ package de.quinscape;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.UUID;
 
 public class Terminal {
-    private String fileNameGenerator() {
-        UUID code = UUID.randomUUID();
-        return code.toString().substring(0, 8);
+
+    private FileWriter file;
+
+    public void appendToFile() throws IOException {
+        printInstructions();
+        createNewFile();
+        writeText();
     }
 
-    private FileWriter createNewFile() throws IOException {
-        return new FileWriter(fileNameGenerator() + ".txt");
-    }
-
-    private FileWriter useGivenFile() throws IOException {
-        return new FileWriter(savedFile(), true);
-    }
-
-    private String savedFile() {
-        return "/Users/pchitanta/Documents/JavaCourseUdemy/Terminal/2cf00dd4.txt";
-    }
-
-    private FileWriter chooseFile() throws IOException{
-        if (savedFile().isEmpty()) {
-            return createNewFile();
-        } else {
-            return useGivenFile();
-        }
+    public void appendToGivenFile(String filename) throws IOException {
+        printInstructions();
+        startWithFileOrCreateNew(filename);
+        writeText();
     }
 
     private void writeText() {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                boolean isOver = true;
-                FileWriter writer = chooseFile();
-                while (isOver) {
-                    String userInput = scanner.nextLine();
-                    if (!userInput.isEmpty()) {
-                        writer.append(userInput).append("\n");
-                    } else {
-                        isOver = false;
-                    }
+        try {
+            Scanner scanner = new Scanner(System.in);
+            boolean isOver = true;
+            while (isOver) {
+                String userInput = scanner.nextLine();
+                if (!userInput.isEmpty()) {
+                    file.append(userInput).append("\n");
+                } else {
+                    isOver = false;
                 }
-                writer.close();
-            } catch (IOException e) {
-                System.out.println("Error: " + e.getMessage());
             }
+            file.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     private void printInstructions() {
         System.out.println("Write your text below: ");
     }
 
-    public void startApplication() {
-        printInstructions();
-        writeText();
+    public void startWithFileOrCreateNew(String filename) throws IOException {
+        if (Files.exists(Path.of(filename))) {
+            try {
+                file = new FileWriter(filename, true);
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } else {
+            createNewFile();
+        }
     }
-//    public void startApplicationWithExistingFile() {
-//    }
-//
-//    public void startProgram(){
-//
-//    }
+
+    private void createNewFile() throws IOException {
+        file = new FileWriter(fileNameGenerator() + ".txt");
+    }
+
+    private String fileNameGenerator() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
 }
